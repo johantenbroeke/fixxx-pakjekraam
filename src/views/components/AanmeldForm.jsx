ed5c6aa03621c7c79f402b7228274a638ebf87cb
Maurice Guikema <unknown>
2019-05-27 08:19:37 +0200

    feat: aanmelden form style init

--- src/views/components/AanmeldForm.jsx
+++ src/views/components/AanmeldForm.jsx
@@ -5,13 +5,13 @@ const { formatDayOfWeek } = require('../../util.js');
 
 class AanmeldForm extends React.Component {
     propTypes = {
-        date: PropTypes.string.isRequired,
+        datum: PropTypes.string.isRequired,
         ondernemer: PropTypes.object.isRequired,
         aanmeldingen: PropTypes.array,
     };
 
     render() {
-        const { aanmeldingen, date, ondernemer } = this.props;
+        const { aanmeldingen, datum, ondernemer } = this.props;
         const rsvp = (aanmeldingen || []).find(a => a.marktDate === date);
         const marktId = rsvp ? rsvp.marktId : '';
         const checked = !!rsvp;
@@ -28,36 +28,49 @@ class AanmeldForm extends React.Component {
 
         return (
             <form method="POST" action="/aanmelden/">
-                <h1>Aanmelden voor sollicitanten</h1>
+                <h1>
+                    Aanmelden voor {ondernemer.voorletters && ondernemer.voorletters + ' '}
+                    {ondernemer.achternaam}
+                </h1>
                 <p>
-                    <label htmlFor="erkenningsnummer">Erkenningsnummer:</label>
-                    <input id="erkenningsnummer" name="erkenningsNummer" defaultValue={ondernemer.erkenningsnummer} />
+                    <label htmlFor="erkenningsNummer">Erkenningsnummer:</label>
+                    <input id="erkenningsNummer" name="erkenningsNummer" defaultValue={ondernemer.erkenningsnummer} />
                 </p>
-                <p>
-                    <label htmlFor="marktId">Markt:</label>
-                    <select name="marktId" id="marktId">
-                        {markten.map(markt => (
-                            <option key={markt.id} value={`${markt.id}`} selected={markt.selected}>
-                                {markt.naam}
-                            </option>
-                        ))}
-                    </select>
-                </p>
-                <p>
-                    <input
-                        id="aanmelding[]"
-                        name="aanmelding"
-                        type="checkbox"
-                        defaultValue={date}
-                        defaultChecked={checked}
-                    />
-                    <label htmlFor="aanmelding[]">
-                        Ik kom morgen ({formatDayOfWeek(date)} {date})
-                    </label>
-                </p>
-                <p>
-                    <input type="submit" />
-                </p>
+                <section className="Fieldset">
+                    <p className="Fieldset">
+                        <label htmlFor="marktId">Markt:</label>
+                        <select name="marktId" id="marktId">
+                            {markten.map(markt => (
+                                <option key={markt.id} value={`${markt.id}`} selected={markt.selected}>
+                                    {markt.naam}
+                                </option>
+                            ))}
+                        </select>
+                    </p>
+                    <p>
+                        <input
+                            id="aanmelding[]"
+                            name="aanmelding"
+                            type="checkbox"
+                            defaultValue={date}
+                            defaultChecked={checked}
+                        />
+                        <label htmlFor="aanmelding[]">
+                            Ik kom morgen ({formatDayOfWeek(date)} {date})
+                        </label>
+                    </p>
+                    <p className="InputField InputField--submit">
+                        <button
+                            className="Button Button--secondary"
+                            type="submit"
+                            name="next"
+                            value={`/markt/${markt.id}/${date}/sollicitanten/#soll-${ondernemer.sollicitatieNummer}`}
+                        >
+                            Opslaan en terug
+                        </button>
+                        <input type="submit" />
+                    </p>
+                </section>
             </form>
         );
     }
