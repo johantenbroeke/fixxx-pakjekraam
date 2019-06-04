821e8c1d3da94816b292597c1528fddbcda00c02
Robbert Broersma <mail@robbertbroersma.nl>
2019-06-04 16:18:17 +0200

    Fix warning about `autocomplete` in Chrome developer tools

--- src/views/components/LoginForm.jsx
+++ src/views/components/LoginForm.jsx
@@ -10,13 +10,19 @@ class LoginForm extends React.Component {
                         <label className="Label" htmlFor="username">
                             Gebruikersnaam:
                         </label>
-                        <input className="Input Input--text" id="username" name="username" />
+                        <input className="Input Input--text" id="username" name="username" autoComplete="username" />
                     </p>
                     <p className="InputField">
                         <label className="Label" htmlFor="password">
                             Wachtwoord:
                         </label>
-                        <input className="Input Input--password" id="password" name="password" type="password" />
+                        <input
+                            className="Input Input--password"
+                            id="password"
+                            name="password"
+                            type="password"
+                            autoComplete="current-password"
+                        />
                     </p>
                     <p className="InputField InputField--submit">
                         <input className="Input Input--submit-primary" type="submit" />
