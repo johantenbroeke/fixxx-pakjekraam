5fe2cbd7436701c994d75f9752762987488cd13b
Robbert Broersma <mail@robbertbroersma.nl>
2019-06-04 16:17:48 +0200

    Marktverordening test

---
+++ src/marktverordening.test.js
@@ -0,0 +1,54 @@
+describe('Marktverordening', () => {
+    describe('Hoofdstuk 3 Markten', () => {
+        describe('Artikel 3.10 Toewijzen losse marktplaatsen bij dagelijkse indeling', () => {
+            describe('1. Voor een losse marktplaats komen, in de volgorde van hun plaats op de marktlijst, in aanmerking:', () => {
+                describe('a. in de eerste plaats: de houders van een vergunning voor een vaste marktplaats;', () => {
+                    it.only('...', () => undefined);
+                });
+                describe('b. in de tweede plaats: de houders van een vergunning voor een tijdelijke vaste marktplaats;', () => {
+                    it.skip('...', () => undefined);
+                });
+                describe('c. in de derde plaats: de sollicitanten.', () => {
+                    it.skip('...', () => undefined);
+                });
+            });
+
+            describe('2. In afwijking van het voorgaande lid, onder c, komen voor een losse marktplaats op een dag- of weekmarkt in aanmerking op vrijdag en zaterdag:', () => {
+                describe('a. in de derde plaats: de sollicitanten die op een andere dag in dezelfde week al een plaats op deze markt hebben ingenomen;', () => {
+                    it.skip('...', () => undefined);
+                });
+                describe('b. in de vierde plaats: de overige sollicitanten, nadat gelegenheid tot het vergroten van marktplaatsen is gegeven.', () => {
+                    it.skip('...', () => undefined);
+                });
+            });
+            describe('3. Bij de toepassing van het eerste lid, onder a en b, vervalt het recht op de vaste marktplaats of de tijdelijke vaste marktplaats voor de duur van de toewijzing van de losse marktplaats, en kan de vaste marktplaats of tijdelijke vaste marktplaats als een losse marktplaats worden uitgegeven.', () => {
+                it.skip('...', () => undefined);
+            });
+            describe('4. Plaatsen die voor de verkoop van bepaalde waren zijn bestemd, worden in de volgorde van de marktlijst eerst toegewezen aan degenen die zich toeleggen op de verkoop van deze waren.', () => {
+                it.skip('...', () => undefined);
+            });
+            describe('5. Plaatsen die zijn aangewezen als plaats voor het handelen vanuit een verkoopinrichting anders dan een kraam, worden in de volgorde van de marktlijst eerst toegewezen aan degenen die gebruik maken van een dergelijke verkoopinrichting.', () => {
+                it.skip('...', () => undefined);
+            });
+            describe('6. Vaste marktplaatsen en tijdelijke vaste marktplaatsen die niet binnen een door het college te bepalen tijdstip na de aanvang van de markt door de rechthebbende zijn bezet, worden voor die dag als losse marktplaatsen uitgegeven.', () => {
+                it.skip('...', () => undefined);
+            });
+        });
+
+        describe('Artikel 3.12 verkleinen en vergroten marktplaatsen bij dagelijkse marktindeling', () => {
+            describe('1. Bij de dagelijkse indeling kunnen, op de marktgedeelten waar het college dit toestaat, toegewezen meer- of tweevoudige marktplaatsen worden verkleind tot twee- of enkelvoudige marktplaatsen.', () => {
+                it.skip('...', () => undefined);
+            });
+
+            describe('2. Het vergroten geschiedt in de volgorde, genoemd in artikel 3.10, eerste en tweede lid, met dien verstande dat bij voorrang de vaste marktplaatshouder in aanmerking komt waarbij de plaatsvergroting aansluit op diens eigen vaste marktplaats.', () => {
+                it.skip('...', () => undefined);
+            });
+            describe('3. Bij de dagelijkse indeling kunnen, op de marktgedeelten waar het college dit toestaat, toe te wijzen enkelvoudige marktplaatsen worden vergroot tot twee- of meervoudige marktplaatsen, mits de betrokken marktplaatsen zich in dezelfde kramenrij bevinden en op elkaar aansluiten en met dien verstande dat een tweevoudige marktplaats pas mag worden vergroot tot een meervoudige marktplaats als alle gegadigden in de gelegenheid zijn geweest een tweevoudige marktplaats te kiezen.', () => {
+                it.skip('...', () => undefined);
+            });
+            describe('4. Er wordt geen gelegenheid tot het vergroten van marktplaatsen gegeven als het aantal open marktplaatsen gelijk is aan of kleiner is dan het aantal sollicitanten voor een losse marktplaats op die dag.', () => {
+                it.skip('...', () => undefined);
+            });
+        });
+    });
+});
