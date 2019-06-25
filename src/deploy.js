f5c6ecff7239c408447bd461f4dd8ffab8756995
Maurice Guikema <unknown>
2019-06-25 14:40:58 +0200

    fix:         force: process.env.NODE_ENV === 'development',

--- src/deploy.js
+++ src/deploy.js
@@ -1,14 +1,10 @@
 const models = require('./model/index.js');
 
 // Ensure the database tables have been created, particularly the session storage.
-models.sequelize
-    .sync({
-        force: process.env.NODE_ENV === 'development',
-    })
-    .then(
-        () => console.log('Database tables successfully initialized'),
-        err => {
-            console.log(err);
-            process.exit(1);
-        },
-    );
+models.sequelize.sync({}).then(
+    () => console.log('Database tables successfully initialized'),
+    err => {
+        console.log(err);
+        process.exit(1);
+    },
+);
