4fce933d8a85f01350a39d8f97d1f949dabc3828
Maurice Guikema <unknown>
2019-09-03 09:11:57 +0200

    fix: aanwezigheid editable after 21:00

--- src/views/components/AfmeldForm.jsx
+++ src/views/components/AfmeldForm.jsx
@@ -32,7 +32,7 @@ class AfmeldForm extends React.Component {
             soll => !soll.doorgehaald && String(soll.markt.id) === currentMarktId
         );
         const markt = markten.find(m => String(m.id) === currentMarktId);
-        const OFFSET = 14; //from 24:00 to 10:00
+        const OFFSET = 14; // from 24:00 to 10:00
         const TZ_CORRECTION = new Date().getTimezoneOffset() / MINUTES_IN_HOUR;
         const now = addMinutes(new Date(), (MINUTES_IN_HOUR * (OFFSET - TZ_CORRECTION)));
 
