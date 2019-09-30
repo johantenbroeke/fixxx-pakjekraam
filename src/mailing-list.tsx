c35fb82ae71f131ec1d7aa1635b3a2595e02e9e7
Tom Ootes <tomootes@gmail.com>
2019-09-30 11:54:57 +0200

    testing mailing list

--- src/mailing-list.tsx
+++ src/mailing-list.tsx
@@ -1,8 +1,8 @@
-import * as React from 'react';
-import { EmailIndeling } from './views/EmailIndeling';
+// import * as React from 'react';
+// import { EmailIndeling } from './views/EmailIndeling';
 import { defer } from 'rxjs';
 import { shareReplay, tap, combineLatest } from 'rxjs/operators';
-import { mail } from './mail.js';
+// import { mail } from './mail.js';
 import { requireEnv, tomorrow } from './util';
 import {
     getAanmeldingen,
@@ -22,7 +22,7 @@ requireEnv('MAILER_FROM');
 const marktDate = tomorrow();
 
 const users$ = defer(() => getAllUsers()).pipe(
-    tap(() => console.log('Keycloak OK!'), () => console.log('Unable to connect to Keycloak')),
+    tap(() => console.log('Keycloak OK!'), (e) => console.log(`Unable to connect to Keycloak: ${e}`)),
     retry(10, 5000),
     shareReplay(1),
 );
@@ -49,75 +49,78 @@ makkelijkeMarkt$.pipe(combineLatest(users$)).subscribe(([makkelijkeMarkt, users]
                     getMarktplaatsen(String(markt.id)),
                     getAllBranches(),
                 ]).then(([ondernemers, toewijzingen, plaatsvoorkeuren, aanmeldingen, marktplaatsen, branches]) => {
+
                     console.log(`Verstuur mails voor de marktindeling van ${markt.id} op datum ${marktDate}`);
                     console.log('Marktondernemers', ondernemers ? ondernemers.length : 0);
                     console.log('Toewijzingen', toewijzingen ? toewijzingen.length : 0);
 
+                    // console.log(ondernemers);
+
                     const registeredUsers = users
-                        .filter(({ username }) =>
-                            ondernemers.some(({ erkenningsNummer }) => erkenningsNummer === username),
-                        )
-                        .filter(user => !!user.email);
+                        .filter( username => ondernemers.some( erkenningsNummer  => erkenningsNummer === username) )
+                        .filter(user => Boolean(user.email));
 
-                    console.log(
-                        'Geregistreerde marktondernemers met e-mail',
-                        registeredUsers ? registeredUsers.length : 0,
-                    );
+                    console.log(registeredUsers);
 
-                    toewijzingen
-                        .map(toewijzing => {
-                            const ondernemer = ondernemers.find(
-                                ({ erkenningsNummer }) => erkenningsNummer === toewijzing.erkenningsNummer,
-                            );
-                            const user = users.find(({ username }) => username === toewijzing.erkenningsNummer);
+                    // console.log(
+                    //     'Geregistreerde marktondernemers met e-mail',
+                    //     registeredUsers ? registeredUsers.length : 0,
+                    // );
 
-                            return {
-                                toewijzing,
-                                ondernemer,
-                                user,
-                            };
-                        })
-                        .filter(({ user }) => !!user && !!user.email)
-                        .map(({ ondernemer, user, toewijzing }) => {
-                            const props = {
-                                markt,
-                                marktplaatsen,
-                                marktDate,
-                                ondernemer,
-                                voorkeuren: plaatsvoorkeuren,
-                                aanmeldingen,
-                                branches,
-                                toewijzing,
-                            };
+                    // toewijzingen
+                    //     .map(toewijzing => {
+                    //         const ondernemer = ondernemers.find(
+                    //             ({ erkenningsNummer }) => erkenningsNummer === toewijzing.erkenningsNummer,
+                    //         );
+                    //         const user = users.find(({ username }) => username === toewijzing.erkenningsNummer);
 
-                            console.log(
-                                `Stuur e-mail naar ${user.email}! Ondernemer is ingedeeld op plaats ${
-                                    toewijzing.plaatsen
-                                }`,
-                            );
+                    //         return {
+                    //             toewijzing,
+                    //             ondernemer,
+                    //             user,
+                    //         };
+                    //     })
+                    //     .filter(({ user }) => !!user && !!user.email)
+                    //     .map(({ ondernemer, user, toewijzing }) => {
+                    //         const props = {
+                    //             markt,
+                    //             marktplaatsen,
+                    //             marktDate,
+                    //             ondernemer,
+                    //             voorkeuren: plaatsvoorkeuren,
+                    //             aanmeldingen,
+                    //             branches,
+                    //             toewijzing,
+                    //         };
 
-                            const subject = `${markt.naam} indeling voor ${marktDate}`;
+                    //         console.log(
+                    //             `Stuur e-mail naar ${user.email}! Ondernemer is ingedeeld op plaats ${
+                    //                 toewijzing.plaatsen
+                    //             }`,
+                    //         );
 
-                            console.log(subject);
+                    //         const subject = `${markt.naam} indeling voor ${marktDate}`;
 
-                            const testEmail = {
-                                from: process.env.MAILER_FROM,
-                                to: user.email,
-                                subject,
-                                react: <EmailIndeling {...props} />,
-                            };
+                    //         console.log(subject);
 
-                            mail(testEmail).then(
-                                () => {
-                                    console.log('E-mail is verstuurd.');
-                                    process.exit(0);
-                                },
-                                (err: Error) => {
-                                    console.error('E-mail sturen mislukt.', err);
-                                    process.exit(1);
-                                },
-                            );
-                        });
+                    //         const testEmail = {
+                    //             from: process.env.MAILER_FROM,
+                    //             to: user.email,
+                    //             subject,
+                    //             react: <EmailIndeling {...props} />,
+                    //         };
+
+                    //         mail(testEmail).then(
+                    //             () => {
+                    //                 console.log('E-mail is verstuurd.');
+                    //                 process.exit(0);
+                    //             },
+                    //             (err: Error) => {
+                    //                 console.error('E-mail sturen mislukt.', err);
+                    //                 process.exit(1);
+                    //             },
+                    //         );
+                    //     });
                 }),
             ),
     ),
