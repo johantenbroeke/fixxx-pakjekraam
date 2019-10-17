e9094a5b79fdbd7d582afc24a9a7f4d9abce2768
Tom Ootes <tomootes@gmail.com>
2019-10-17 14:59:40 +0200

    Added .sql examples

---
+++ examples.sql
@@ -0,0 +1,2 @@
+ALTER TABLE plaatsvoorkeur DROP CONSTRAINT "plaatsvoorkeur_marktId_erkenningsNummer_plaatsId_key";
+ALTER TABLE voorkeur DROP CONSTRAINT "voorkeur_erkenningsNummer_marktId_marktDate_key";
