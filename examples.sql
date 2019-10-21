109a96b47c5263cea61f00fb4ea416e929c05834
Tom Ootes <tomootes@gmail.com>
2019-10-21 13:41:37 +0200

    First delete double accounts

--- examples.sql
+++ examples.sql
@@ -1,2 +1,9 @@
-ALTER TABLE plaatsvoorkeur DROP CONSTRAINT "plaatsvoorkeur_marktId_erkenningsNummer_plaatsId_key";
-ALTER TABLE voorkeur DROP CONSTRAINT "voorkeur_erkenningsNummer_marktId_marktDate_key";
+ALTER TABLE plaatsvoorkeur DROP CONSTRAINT "marktid_erkenningsnummer_plaatsid_key";
+ALTER TABLE voorkeur DROP CONSTRAINT "voorkeur_erkenningsnummer_marktid_marktdate_key";
+
+-- Add contraint
+ALTER TABLE plaatsvoorkeur ADD CONSTRAINT marktId_erkenningsNummer_plaatsId_key UNIQUE ("marktId", "erkenningsNummer", "plaatsId");
+ALTER TABLE voorkeur ADD CONSTRAINT voorkeur_erkenningsnummer_marktid_marktdate_key UNIQUE ("marktId", "erkenningsNummer", "marktDate");
+
+-- Find with nummer
+SELECT * FROM plaatsvoorkeur WHERE "erkenningsNummer" = 'erkenningsNummer';
