58b2914fdfcb71a59f1c7ca7fa202c4f4eb1590c
Daan Mortier <tjoekbezoer@gmail.com>
2019-11-18 14:35:46 +0100

    Algorithm: Small change

--- src/allocation/indeling.ts
+++ src/allocation/indeling.ts
@@ -52,10 +52,9 @@ const Indeling = {
         }
 
         const openPlaatsen = markt.marktplaatsen.filter(plaats => !plaats.inactive);
-        const expansionLimit = Math.min(
-            Number.isFinite(markt.expansionLimit) ? markt.expansionLimit : Infinity,
-            markt.marktplaatsen.length
-        );
+        const expansionLimit = Number.isFinite(markt.expansionLimit) ?
+                               markt.expansionLimit :
+                               markt.marktplaatsen.length;
 
         const indeling = <IMarktindeling> {
             ...markt,
