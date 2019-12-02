4e9524b707862cd78960f39b4a627c389c68b60b
Tom Ootes <tomootes@gmail.com>
2019-12-02 14:08:58 +0100

    geblokkeerde plaatsen (588) en keycloack templates (506)

--- src/views/MarktDetailPage.jsx
+++ src/views/MarktDetailPage.jsx
@@ -46,8 +46,12 @@ class MarktDetailPage extends React.Component {
         let fase = null;
         markt.kiesJeKraamFase == 'live' ? fase = null : fase = ` ${markt.kiesJeKraamFase}`;
 
+
         return (
             <MarktDetailBase bodyClass="page-markt-detail" datum={datum} type={type} markt={markt} fase={fase}>
+                 {/* {markt.kiesJeKraamGeblokkeerdePlaatsen ?
+                 <p>Geblokkeerde plaatsen: {markt.kiesJeKraamGeblokkeerdePlaatsen}</p> :
+                 null } */}
                 <div className="row row--responsive Section">
                     <a href={`./langdurig-afgemeld/`} className="Link">Ondernemers langdurig afgemeld</a>
                 </div>
