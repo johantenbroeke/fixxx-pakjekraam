8c2ddc3fbc1847a492124f91cb1de4f585ac87c9
Daan Mortier <tjoekbezoer@gmail.com>
2020-05-12 13:43:30 +0200

    Types: Nieuwe types worden ondersteund in `OndernemerStatus` component

--- src/views/components/SollicitatieSpecs.jsx
+++ src/views/components/SollicitatieSpecs.jsx
@@ -1,14 +1,28 @@
 const PropTypes = require('prop-types');
 const React = require('react');
 
+const {
+    isVast,
+    isTVPLZ
+} = require('../../domain-knowledge.js');
+
 const SollicitatieSpecs = ({ markt, sollicitatie }) => {
+    const aantalPlaatsen = sollicitatie.vastePlaatsen.length;
+    const vastePlaatsen = aantalPlaatsen ?
+                          sollicitatie.vastePlaatsen.join(', ') :
+                          '';
+
     return (
         <div className="SollicitatieSpecs">
             <span className={`Pil Pil--${sollicitatie.status}`}>{sollicitatie.status}</span>
             <span className="Pil">sollnr. {sollicitatie.sollicitatieNummer}</span>
-            {sollicitatie.vastePlaatsen.length > 0 ? (
+
+            {aantalPlaatsen > 0 ? (
                 <span className="Pil">
-                    plaats{sollicitatie.vastePlaatsen.length > 1 ? `en` : ``} {sollicitatie.vastePlaatsen.join(', ')}
+                    { isTVPLZ(sollicitatie.status) ?
+                         `${aantalPlaatsen} plaats${aantalPlaatsen > 1 ? 'en' : ''}`:
+                         `plaats${aantalPlaatsen > 1 ? 'en' : ''} ${vastePlaatsen}`
+                    }
                 </span>
             ) : null}
         </div>
